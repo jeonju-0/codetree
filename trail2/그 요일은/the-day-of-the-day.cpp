3d3030935bbb89aeb11365669334f9b6f04ee17b
이상민 <wjswnpk@gmail.com>
2026-09-06 21:41:34 +0900

    [Passed] 그 요일은 (2ms, 0MB)

--- trail2/그 요일은/the-day-of-the-day.cpp
+++ trail2/그 요일은/the-day-of-the-day.cpp
@@ -5,6 +5,16 @@ using namespace std;
 int num_of_days[13] = {-99, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 string day_name[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
+int ordinalDate(int m, int d) {
+    int res = 0;
+    for (int i = 1; i < m; ++i) {
+        res += num_of_days[i];
+    }
+    res += d;
+
+    return res;
+}
+
 int main() {
     int m1, d1, m2, d2;
     cin >> m1 >> d1 >> m2 >> d2;
@@ -15,22 +25,9 @@ int main() {
         if (target_day == day_name[i]) {target_day_idx = i; break;}
     }
 
-    int cnt = 0;
-    int curr_month = m1;
-    int curr_date = d1;
-    int curr_day_idx = 0;
-    while (!(curr_month == m2 && curr_date == d2)) {
-        if (curr_day_idx == target_day_idx) {cnt++;}
-        if (curr_date == num_of_days[curr_month]) {
-            curr_month++;
-            curr_date = 1;
-        }
-        else {
-            curr_date++;
-        }
-        curr_day_idx = (curr_day_idx + 1)%7;
-    }
-    if (curr_day_idx == target_day_idx) {cnt++;}
+    int total_days = ordinalDate(m2, d2) - ordinalDate(m1, d1) + 1;
+    int cnt = total_days/7;
+    cnt += (target_day_idx <= total_days%7 - 1);
     cout << cnt;
 
     return 0;
